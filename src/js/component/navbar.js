05db8f86a5d0f0200e83409711fb702c24f814fa
Ezequiel <ezequieljfrias20@gmail.com>
2021-03-22 16:11:03 -0400

    listo el home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.scss";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+		<nav className="fill">
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Downloads</a>
+				</li>
+				<li>
+					<a href="#">Iniciar Sesión</a>
+				</li>
+				<li>
+					<a href="#">Regístrate</a>
+				</li>
+			</ul>
 		</nav>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Navbar } from "../component/navbar";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+	<>
+		<div className="container-fluid background-home">
+			<div className="row">
+				<div className="col-md-12 col-sm-12">
+					<Navbar />
+				</div>
+			</div>
+			<div className="row botones-home">
+				<div className="col-md-6 botones-home justify-content-end">
+					<span className="boton3">Entrar</span>
+				</div>
+				<div className="col-md-6 botones-home">
+					<span className="boton4">Registrate</span>
+				</div>
+			</div>
+		</div>
+	</>
 );
